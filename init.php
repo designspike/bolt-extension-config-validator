27a87f277c5288f93d9fe800fc5abb16ada30075
Will Hall <will@designspike.com>
2016-06-02 12:02:56 -0700

    Convert to Bolt 3 compatibility

--- init.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Bolt\Extension\DesignSpike\ConfigValidator;
-
-if (isset($app)) {
-    $app['extensions']->register(new Extension($app));
-}
--- src/ConfigValidatorExtension.php
+++ src/ConfigValidatorExtension.php
@@ -2,39 +2,42 @@
 
 namespace Bolt\Extension\DesignSpike\ConfigValidator;
 
-use Bolt;
+use Bolt\Extension\SimpleExtension;
 use RomaricDrigon\MetaYaml\MetaYaml;
 use Symfony\Component\Yaml\Parser;
 use Symfony\Component\HttpFoundation\Response;
+use Bolt\Menu\MenuEntry;
+use Silex\ControllerCollection;
+use Silex\Application;
 
-class Extension extends \Bolt\BaseExtension
+class ConfigValidatorExtension extends SimpleExtension
 {
-
-    public  $config;
-
-    private $configDirectory;
-    private $schemaDirectory;
-    private $path;
-
-    const NAME = "ConfigValidator";
-
-    public function initialize()
+    protected function registerMenuEntries()
     {
-        $this->configDirectory = $this->app['resources']->getPath('config');
-        $this->schemaDirectory = __DIR__ . '/schemas';
+        $menu = new MenuEntry('config-validator-menu', 'config-validator');
+        $menu->setLabel('Validate config files')
+            ->setIcon('fa:check')
+            ->setPermission('settings')
+        ;
 
-        $this->path = $this->app['config']->get('general/branding/path') . '/extensions/config-validator';
-        $this->app->match($this->path, [$this, 'ConfigValidator']);
-
-        // Add template namespace to twig
-        $this->app['twig.loader.filesystem']->addPath(__DIR__.'/views', 'ConfigValidator');
-
-        // Add menu item
-        $this->addMenuOption('Validate config files', $this->app['resources']->getUrl('bolt') . 'extensions/config-validator', 'fa:check');
-
+        return [
+            $menu,
+        ];
     }
 
-    public function ConfigValidator()
+    protected function registerTwigPaths()
+    {
+        return [
+            'views' => ['position' => 'prepend', 'namespace' => 'ConfigValidator']
+        ];
+    }
+
+    protected function registerBackendRoutes(ControllerCollection $collection)
+    {
+        $collection->match("/extend/config-validator", [$this, 'callbackConfigValidator']);
+    }
+
+    public function callbackConfigValidator(Application $app)
     {
         // Config file to validate => schema file to use
         // TODO: scan extensions to see if any of them requests config validation
@@ -52,23 +55,23 @@ class Extension extends \Bolt\BaseExtension
         $yaml_parser = new Parser();
         foreach ($tests as $test) {
             // Create yaml validator based on a yaml schema document
-            $schema_path = $this->schemaDirectory . '/' . $test['schema_doc'];
+            $schema_path = __DIR__ . '/../schemas/' . $test['schema_doc'];
             $schema_data = $yaml_parser->parse(file_get_contents($schema_path));
             $validator = new MetaYaml($schema_data);
 
             // Get the config data directly from .yml files rather than the config in memory, because live config will
             // not keep unrecognized keys, and the user should know about such keys
-            $config_path = $this->configDirectory . '/' . $test['config_file'];
+            $config_path = $app['resources']->getPath('config') . '/' . $test['config_file'];
 
             // See if the file exists and is writable
             if (!file_exists($config_path)) {
 
-                $this->app['session']->getFlashBag()->add('warning',
+                $app['session']->getFlashBag()->add('warning',
                     $test['config_file'] . " doesn't exist (" . $config_path . ")");
 
             } else if (file_exists($config_path) and ! is_readable($config_path)) {
 
-                $this->app['session']->getFlashBag()->add('error',
+                $app['session']->getFlashBag()->add('error',
                     $test['config_file'] . " exists but isn't readable (" . $config_path . ")");
 
             } else {
@@ -82,21 +85,15 @@ class Extension extends \Bolt\BaseExtension
                     // Try and validate the config
                     $result = $validator->validate($config_data);
                     if ($result === true) {
-                        $this->app['session']->getFlashBag()->add('success', $test['config_file'] . ' valid.');
+                        $app['session']->getFlashBag()->add('success', $test['config_file'] . ' valid.');
                     }
                 } catch (\Exception $e) {
-                    $this->app['session']->getFlashBag()->add('error',
+                    $app['session']->getFlashBag()->add('error',
                         $test['config_file'] . ' is invalid: ' . $e->getMessage());
                 }
             }
         }
 
-        return new Response($this->app['twig']->render('@ConfigValidator/base.twig'));
+        return new Response($app['twig']->render('@ConfigValidator/base.twig'));
     }
-
-    public function getName()
-    {
-        return Extension::NAME;
-    }
-
 }
